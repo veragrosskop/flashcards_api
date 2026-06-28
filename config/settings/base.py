d6f275eb12483da1c3196e2a2aa6d3f0da671aad
veragrosskop <veragrosskop@gmail.com>
2026-06-28 16:20:43 +0200

    Add CI pipeline with black and tests

--- config/settings/base.py
+++ config/settings/base.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-load_dotenv()
+if os.path.exists(BASE_DIR / ".env"):
+    load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
